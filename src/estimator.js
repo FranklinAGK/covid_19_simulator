77932160eb39c943d212c5be917ca27eb21b23aa
Franklin Ejike <Ejike.franklin@ymail.com>
2020-04-14 22:42:54 +0100

    1.1.8

--- src/estimator.js
+++ src/estimator.js
@@ -1,36 +1,36 @@
-const express =  require('express');
-const fs =  require('fs');
-const app =  express();
-const readline =  require('readline').createInterface({
-  input: process.stdin,
-  output: process.stdout
+const express = require('express');
+const fs = require('fs');
+const app = express();
+const readline = require('readline').createInterface({
+ input: process.stdin,
+ output: process.stdout
 })
 
-const covid19ImpactEstimator =  (data)  =>
+const covid19ImpactEstimator = (data) =>
 {
-	const currentlyInfected=  data.reportedCases  * 10;
-	const severeImpactCurrentlyInfected=  data.reportedCases  * 50;
-	// demo day for testing is 58 as ref in the data json object,  this can be changed later. 
+	const currentlyInfected= data.reportedCases * 10;
+	const severeImpactCurrentlyInfected= data.reportedCases * 50;
+	// demo day for testing is 58 as ref in the data json object, this can be changed later. 
 	const inputDays= data.timeToElapse;
 	const sample= Math.floor(inputDays / 3);
 	//convert to whole number
 	const power= 1;
 	for (const i= 1; i <= sample; ++i)
-        {
-             power= power * 2;
-             console.log(power);
-        }
-	const infectionsByRequestedTime=  Math.floor(currentlyInfected * power);
-	const infectionsByRequestedTime_severe=  Math.floor(severeImpactCurrentlyInfected * power);
+    {
+       power= power * 2;
+       console.log(power);
+    }
+	const infectionsByRequestedTime= Math.floor(currentlyInfected * power);
+	const infectionsByRequestedTime_severe= Math.floor(severeImpactCurrentlyInfected * power);
 	//15% is also 
 	const severeCasesByRequestedTime= Math.floor(0.15 * infectionsByRequestedTime);
 	const severeCasesByRequestedTime_severe= Math.floor(0.15 * infectionsByRequestedTime_severe);
 
 	//Free beds space impact/ severe
-	//according to data 35% of beds are extimated to be free 35% =  0.35;
+	//according to data 35% of beds are extimated to be free 35% = 0.35;
 	const freeBeds= Math.floor(0.35 * data.totalHospitalBeds);
-	const hospitalBedsByRequestedTime=  freeBeds-severeCasesByRequestedTime;
-	const hospitalBedsByRequestedTime_severe=  freeBeds-severeCasesByRequestedTime_severe;
+	const hospitalBedsByRequestedTime= freeBeds-severeCasesByRequestedTime;
+	const hospitalBedsByRequestedTime_severe= freeBeds-severeCasesByRequestedTime_severe;
 	//ICU
 	const casesForICUByRequestedTime= Math.floor(0.05 * infectionsByRequestedTime);
 	const casesForICUByRequestedTime_severe= Math.floor(0.05 * infectionsByRequestedTime_severe);
@@ -79,34 +79,34 @@ const covid19ImpactEstimator =  (data)  =>
 collectData = () =>
 {
 	console.log('\nStarting COVID-19 estimator....\n');
-	readline.question('Country/continent name ?\n', (regionName)  => {
-  readline.question('Average Age ?\n', (avAge)  => {
-  readline.question('Average income(in dollars) ?\n', (avIncome)  => {
-  readline.question('Average Daily Income Population(in dollars) ?\n', (AvDailyIncomePopulation)  => {
-  readline.question('Enter Period type (days, weeks , months) ?\n', (type)  => {
-  readline.question('Time To Elapse ?\n', (days)  => {
-  readline.question('Reported cases ?\n', (repCases)  => {
-  readline.question('Total population ?\n', (populationz)  => {
-  readline.question('Total hopital beds ?\n', (totalBeds)  => {
+	readline.question('Country/continent name ?\n', (regionName)=> {
+ readline.question('Average Age ?\n', (avAge)=> {
+ readline.question('Average income(in dollars) ?\n', (avIncome)=> {
+ readline.question('Average Daily Income Population(in dollars) ?\n', (AvDailyIncomePopulation) => {
+ readline.question('Enter Period type (days, weeks , months) ?\n', (type)=> {
+ readline.question('Time To Elapse ?\n', (days)=> {
+ readline.question('Reported cases ?\n', (repCases)=> {
+ readline.question('Total population ?\n', (populationz)=> {
+ readline.question('Total hopital beds ?\n', (totalBeds)=> {
 
-  	switch (type) 
+ 	switch (type) 
 	{
-  case 'days':
+ case 'days':
 	
 	a= days;
-    break;
-  case 'weeks':
-  	//There would always be 7 days in a week.
-  	a= days * 7;
+  break;
+ case 'weeks':
+ 	//There would always be 7 days in a week.
+ 	a= days * 7;
 
-    break;
-  case 'months':
-  	//from description, assume that there would always be 30 days in a month.
-  	a= days * 30;
-    break;
+  break;
+ case 'months':
+ 	//from description, assume that there would always be 30 days in a month.
+ 	a= days * 30;
+  break;
 }
 
-  const data= {
+ const data= {
 region: {
 name: regionName,
 avgAge: Math.floor(avAge),
@@ -121,24 +121,24 @@ totalHospitalBeds: Math.floor(totalBeds)
 };
 console.log(data);
 
-  readline.close();
+ readline.close();
 
 covid19ImpactEstimator(data);
 
 
 });
 });
-  
+ 
 });
-  
+ 
 });
-  
+ 
 });
-  
+ 
 });
-  
+ 
 });
-  
+ 
 });
 
 });
@@ -150,18 +150,18 @@ startServer = (data) =>
 
 const x= JSON.stringify(data);
 app.post('/api/v1/on-covid-19', function (req, res) {
-  res.send(x);
-  console.log(x);
+ res.send(x);
+ console.log(x);
 });
 
 //covid19ImpactEstimator(data);
 
 
 
-app.get('/api/v1/on-covid-19', (req, res, next)  => {
+app.get('/api/v1/on-covid-19', (req, res, next)=> {
  res.json(data);
  
- const start =  new Date();
+ const start = new Date();
  ms= new Date() - start + ' ms';
  //console.log(data);
  console.log('Request took:', ms);
@@ -171,10 +171,10 @@ app.get('/api/v1/on-covid-19', (req, res, next)  => {
  
 });
 
-app.get('/api/v1/on-covid-19/logs', (req, res, next)  => {
+app.get('/api/v1/on-covid-19/logs', (req, res, next)=> {
  res.json(data);
  
- const start2 =  new Date();
+ const start2 = new Date();
  ms= new Date() - start2 + ' ms';
  //console.log(data);
  const x2= '\nGet\t\t /api/v1/on-covid-19/logs \t\t200\t\t'+ms+'\t\t';
@@ -183,10 +183,10 @@ app.get('/api/v1/on-covid-19/logs', (req, res, next)  => {
  
 });
 
-app.get('/api/v1/on-covid-19/xml', (req, res, next)  => {
+app.get('/api/v1/on-covid-19/xml', (req, res, next)=> {
  res.json(data);
  
- const start3 =  new Date();
+ const start3 = new Date();
  ms= new Date() - start3 + ' ms';
  //console.log(data);
  const x3= '\nGet\t\t /api/v1/on-covid-19/xml \t\t200\t\t'+ms+'\t\t';
@@ -195,7 +195,7 @@ app.get('/api/v1/on-covid-19/xml', (req, res, next)  => {
  
 });
 
-app.listen(8080, ()  => {
+app.listen(8080, ()=> {
  console.log('Server running on port 8080');
 });
 
@@ -203,7 +203,7 @@ app.listen(8080, ()  => {
 
 
 collectData();
-exports =  covid19ImpactEstimator;
+exports= covid19ImpactEstimator;
 
 //return covid19ImpactEstimator(data);
 
